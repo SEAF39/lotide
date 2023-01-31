d84a7da115b71f3e43d0bae985a19adc1a5737fd
SEAF39 <seaf.roshi.37@gmail.com>
2023-01-31 05:16:02 +0000

    Tail test case function makes sure elemnts are matched

---
+++ tail.js
@@ -0,0 +1,24 @@
+
+/*
+This test case checks the elements in the returned array from the tail function and makes sure they match the expected values. The first assertEqual statement checks that the length of the result array is equal to 2, as it should contain two elements. The second and third assertEqual statements check that the first and second elements of the result array respectively match the expected values of "Lighthouse" and "Labs".
+
+*/
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
