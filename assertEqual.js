9afcad96d1325443dba8fc81d5c9ac68670f9cae
SEAF39 <seaf.roshi.37@gmail.com>
2023-01-31 04:23:09 +0000

    The console.assert function tests whether the provided expression SumBuggy

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+/*
+
 // FUNCTION IMPLEMENTATION
 const sum = function(a, b) { 
   return a + b;
@@ -6,3 +8,13 @@ const sum = function(a, b) {
 // TEST CODE
 console.assert(sum(1, 2) === 3);
 console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+
+*/
+
+// FUNCTION IMPLEMENTATION
+const sumBuggy = function(a, b) { 
+  return a * b;
+}
+
+// TEST CODE
+console.assert(sumBuggy(1, 2) === 3); // fails, because bug! The expected output should be 3 but it returns 2 instead. This is because the function is multiplying a and b instead of adding them.
