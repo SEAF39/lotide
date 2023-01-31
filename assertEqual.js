ac9c0df19bc968762731923d230f65ea2eca3cd7
SEAF39 <seaf.roshi.37@gmail.com>
2023-01-31 04:33:17 +0000

    The console.assert function tests between two expression

--- assertEqual.js
+++ assertEqual.js
@@ -9,7 +9,7 @@ const sum = function(a, b) {
 console.assert(sum(1, 2) === 3);
 console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
 
-*/
+
 
 // FUNCTION IMPLEMENTATION
 const sumBuggy = function(a, b) { 
@@ -18,3 +18,22 @@ const sumBuggy = function(a, b) {
 
 // TEST CODE
 console.assert(sumBuggy(1, 2) === 3); // fails, because bug! The expected output should be 3 but it returns 2 instead. This is because the function is multiplying a and b instead of adding them.
+
+*/
+
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+  
+  
