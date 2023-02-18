0315e126ae995bffef993d5c6ad4962383797948
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-17 20:45:01 -0500

    - Lotide Refactor -AssertEqualTest - MOUDEL 1- WEEK- 4

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+
+// read the test file from assertEquals function
+const assertEqual = require('../assertEqual');
+
+
+// refactoring the code based on Lotide Refactor Example
+module.exports = assertEqual;
