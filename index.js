ea9a6c5e39a58957cc732b3cf660fd2f91680c7f
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-18 01:20:19 -0500

    - export all functions - MOUDEL 1- WEEK- 4

---
+++ index.js
@@ -0,0 +1,39 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const flatten = require('./flatten')
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual,
+  without,
+  takeUntil,
+  map,
+  letterPositions,
+  flatten,
+  countOnly,
+  countLetters,
+  findKey,
+  findKeyByValue,
+  eqObjects,
+  assertObjectsEqual
+
+};
