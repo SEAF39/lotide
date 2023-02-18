d7c792819dd0801d8294e708a9e17d534f6c6b75
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-18 01:07:53 -0500

    - headTest assignment with mocha & chai - MOUDEL 1- WEEK- 4

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,20 @@
-// read the test file from head function
-const head = require('../head.js');
+// Import the 'assert' function from the Chai library
+const assert = require('chai').assert;
 
+// Import the 'head' function from another file
+const head   = require('../head');
 
-// refactoring the code based on Lotide Refactor Example
-module.exports = head;
+// Start a test suite for the 'head' function
+describe("#head", () => {
+  // Start the first test case
+  it("returns 1 for [1, 2, 3]", () => {
+    // Use 'assert.strictEqual' to compare the output of 'head([1, 2, 3])' with the expected output of '1'
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  // Start the second test case
+  it("returns '5' for ['5']", () => {
+    // Use 'assert.strictEqual' to compare the output of 'head(['5'])' with the expected output of ''5''
+    assert.strictEqual(head(['5']), '5');
+  });
+});
