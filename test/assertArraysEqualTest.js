13c588dfe608d99208baeff551623ec72b6903a2
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-17 21:50:36 -0500

    - Lotide Refactor - assertArraysEqualTest - MOUDEL 1- WEEK- 4

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+// read the test from assertArraysEqual.js file
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+
+// refactoring the code based on Lotide Refactor Example
+module.exports = assertArraysEqual;
