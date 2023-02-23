ed2f7892b27e933719470125f75ff9e53541ce32
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-23 01:54:29 -0500

    - Code Updated & organized - MOUDEL 1- WEEK- 4

--- without.js
+++ without.js
@@ -1,37 +1,57 @@
-const assertArraysEqual = function(actual, expected) {
-  // check if the length of the arrays are equal
-  if (actual.length !== expected.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    return;
-  }
-  
-  // loop through the arrays to check if each element is equal
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-      return;
-    }
-  }
-  
-  // if the arrays are equal, print success message
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-};
 
+// create a new array to store the elements not present in itemsToRemove
+// loop through the source array
+// check if the current element is not present in itemsToRemove
+// return the newArray
+// if it's not, add it to the newArray
+      
 const without = function(source, itemsToRemove) {
-  // create a new array to store the elements not present in itemsToRemove
-  let newArray = [];
-  
-  // loop through the source array
+  let newArray = [];  
   for (let i = 0; i < source.length; i++) {
-    // check if the current element is not present in itemsToRemove
     if (!itemsToRemove.includes(source[i])) {
-      // if it's not, add it to the newArray
       newArray.push(source[i]);
     }
   }
-  
-  // return the newArray
   return newArray;
+};
+
+// Check if the lengths of the arrays are not equal, return false
+// Iterate through each element in the arrays
+// Check if both elements are arrays and if eqArrays returns false
+// Check if the elements are not arrays and if they are not equal
+// Return true if all elements match
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length  !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (Array.isArray(arrayOne[i]) && (eqArrays(arrayOne[i], arrayTwo[i]) === false)) {
+      return false;
+    }
+    if (!(Array.isArray(arrayOne[i])) && arrayOne[i] !== arrayTwo[i]){
+      return false;
+    }
+  }
+  return true 
+};
+
+// check if the length of the arrays are equal
+// loop through the arrays to check if each element is equal
+// if the arrays are equal, print success message
+    
+const assertArraysEqual = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }  
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
 const words = ["hello", "world", "lighthouse"];
