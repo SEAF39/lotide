9edfa25b7132eda0ad8963e1be029d76bfa406b7
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-23 02:23:46 -0500

    - Code Updated & organized - MOUDEL 1- WEEK- 4

--- test/headTest.js
+++ test/headTest.js
@@ -1,20 +1,18 @@
 // Import the 'assert' function from the Chai library
-const assert = require('chai').assert;
-
 // Import the 'head' function from another file
-const head   = require('../head');
-
 // Start a test suite for the 'head' function
+// Start the first test case
+// Use 'assert.strictEqual' to compare the output of 'head([1, 2, 3])' with the expected output of '1'
+// Start the second test case
+// Use 'assert.strictEqual' to compare the output of 'head(['5'])' with the expected output of ''5''
+
+const assert = require('chai').assert;
+const head   = require('../head');
 describe("#head", () => {
-  // Start the first test case
   it("returns 1 for [1, 2, 3]", () => {
-    // Use 'assert.strictEqual' to compare the output of 'head([1, 2, 3])' with the expected output of '1'
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-
-  // Start the second test case
   it("returns '5' for ['5']", () => {
-    // Use 'assert.strictEqual' to compare the output of 'head(['5'])' with the expected output of ''5''
     assert.strictEqual(head(['5']), '5');
   });
 });
