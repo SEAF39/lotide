479c40633235235baa817357ef2e747f889e681a
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-22 23:34:21 -0500

    - Fix the code based on Instructor comments  - MOUDEL 1- WEEK- 4

--- assertEqual.js
+++ assertEqual.js
@@ -5,9 +5,9 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ The following: Assertion Passed: " + actual + " === " + expected);
+    console.log("✅✅✅✅ The following: Assertion Passed: " + actual + " === " + expected);
   } else {
-    console.log("❌ The following: Assertion Failed: " + actual + " !== " + expected);
+    console.log("❌❌❌❌ The following: Assertion Failed: " + actual + " !== " + expected);
   }
 };
 
