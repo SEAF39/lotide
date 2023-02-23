7d576b59da44e528ffde65577185b75799377c07
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-23 00:47:25 -0500

    - Code Updated & organized - MOUDEL 1- WEEK- 4

--- eqObjects.js
+++ eqObjects.js
@@ -1,54 +1,59 @@
+// FUNCTION IMPLEMENTATION
+
+// Compare the actual and expected values, and log a message to the console indicating whether they are equal or not
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅✅ The following: Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑🛑 The following: Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// Check if the lengths of both arrays are equal
+// Loop through each element of both arrays and compare them
+// If all elements are equal, return true
+    
 const eqArrays = function(array1, array2) {
-  // Check if the lengths of both arrays are equal
   if (array1.length !== array2.length) {
     return false;
   }
-  // Loop through each element of both arrays and compare them
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
-  // If all elements are equal, return true
   return true;
 };
 
-const assertEqual = function(actual, expected) {
-  // Compare the actual and expected values, and log a message to the console indicating whether they are equal or not
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// Get the keys of both objects
+// If the lengths of the keys are not equal, return false
+// Loop through each key of the first object
+// If the second object does not have the key, return false
+// If the value of the key is an array, use eqArrays to compare the arrays
+// If the value of the key is not an array, compare the values using ===
+// If all checks have passed, return true
 
 const eqObjects = function(object1, object2) {
-  // Get the keys of both objects
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
-  // If the lengths of the keys are not equal, return false
   if (keys1.length !== keys2.length) {
     return false;
   }
-  // Loop through each key of the first object
   for (const key of keys1) {
-    // If the second object does not have the key, return false
     if (!keys2.includes(key)) {
       return false;
     }
-    // If the value of the key is an array, use eqArrays to compare the arrays
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     } else {
-      // If the value of the key is not an array, compare the values using ===
       if (object1[key] !== object2[key]) {
         return false;
       }
     }
   }
-  // If all checks have passed, return true
   return true;
 };
 
@@ -57,11 +62,6 @@ const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+const bca = { b: "2", c: "3", a: "1" };
+assertEqual(eqObjects(abc, bca), false); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
